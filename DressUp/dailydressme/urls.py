5bdf0f0e456532f502a7faa70b553ae2b42e69d9
firasnoun <fbn02@mail.aub.edu>
2024-04-03 15:44:07 +0300

    vxvxv

--- DressUp/dailydressme/urls.py
+++ DressUp/dailydressme/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path('get-outfit/', views.outfit_recommendation_view, name='outfit_recommendation'),
     path('', views.index, name='index'),
+    path('api/outfit-recommendation/', views.OutfitRecommendationView.as_view(), name='outfit-recommendation'),
 ]
 
