50c3df199db2eb67dd8b4746b2ede09443867fbc
firasnoun <fbn02@mail.aub.edu>
2024-04-03 11:08:47 +0300

    deleted

--- DressUp/DressUp/urls.py
+++ DressUp/DressUp/urls.py
@@ -23,7 +23,5 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dailydressme.urls')),
-    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 ]
 urlpatterns += staticfiles_urlpatterns()
