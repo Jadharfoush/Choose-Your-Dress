58b5c892c3afb51e786467c91fe7403fa78c362e
firasnoun <fbn02@mail.aub.edu>
2024-04-03 10:15:41 +0300

    jnxc

--- DressUp/dailydressme/urls.py
+++ DressUp/dailydressme/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 urlpatterns = [
     path('get-outfit/', views.outfit_recommendation_view, name='outfit_recommendation'),
-    path('DressUp\DressUp\templates\index.html', views.index, name='index'),
+    path('index', views.index, name='index'),
 ]
 
