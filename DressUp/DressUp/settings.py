b207204c7474d9ffc1c0d0a1405fb724d319b889
firasnoun <fbn02@mail.aub.edu>
2024-04-03 11:36:46 +0300

    fixing

--- DressUp/DressUp/settings.py
+++ DressUp/DressUp/settings.py
@@ -124,10 +124,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    
-]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
