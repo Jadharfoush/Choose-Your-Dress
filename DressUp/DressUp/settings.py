0d668ccdcac82ba1828dfe470c8cfa67c19aceb8
firasnoun <fbn02@mail.aub.edu>
2024-04-03 11:45:44 +0300

    jdsnhfjx

--- DressUp/DressUp/settings.py
+++ DressUp/DressUp/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'DressUp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'DressUp/templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
