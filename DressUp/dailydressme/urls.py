47c5f702956d264aa25a7478c85c961cf537a00f
firasnoun <fbn02@mail.aub.edu>
2024-04-03 15:45:48 +0300

    xvxv

--- DressUp/dailydressme/urls.py
+++ DressUp/dailydressme/urls.py
@@ -4,7 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('get-outfit/', views.outfit_recommendation_view, name='outfit_recommendation'),
     path('', views.index, name='index'),
     path('api/outfit-recommendation/', views.OutfitRecommendationView.as_view(), name='outfit-recommendation'),
 ]
