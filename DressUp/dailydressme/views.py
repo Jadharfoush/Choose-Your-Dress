08b789b4f2d13d33e0ee76bd051a96f6b27d8dbe
firasnoun <fbn02@mail.aub.edu>
2024-04-09 13:40:54 +0300

    sxvs

--- DressUp/dailydressme/views.py
+++ DressUp/dailydressme/views.py
@@ -66,9 +66,10 @@ class OutfitRecommendationAPIView(APIView):
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
 class TemperatureAPIView(APIView):
-    def post(city,request, *args, **kwargs): # Default to Beirut if no city is provided
+    def post(self, request, *args, **kwargs):
+        city = request.data.get('city', 'Beirut')  # Default to Beirut if no city is provided
         try:
             temperature = get_weather_data(city)
-            return Response({'temperature': temperature})
+            return Response({'city': city, 'temperature': temperature})
         except requests.RequestException as e:
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
