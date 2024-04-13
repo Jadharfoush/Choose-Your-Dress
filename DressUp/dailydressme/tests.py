01f116ffa11d954627ca82d018794c75e635543c
Ubuntu <ubuntu@ip-172-31-25-35.eu-central-1.compute.internal>
2024-04-13 16:16:58 +0000

    added tests.py for validation

--- DressUp/dailydressme/tests.py
+++ DressUp/dailydressme/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+import requests
 
-# Create your tests here.
+class TemperatureAPITest(TestCase):
+    def test_temperature_equality(self):
+        # Fetch temperature from custom API
+        response_custom = requests.post('http://3.121.199.121:8000/api/temperature', json={"city": "Oslo"})
+        temp_custom = response_custom.json()['temperature']
+
+        # Fetch temperature from OpenWeatherMap
+        response_owm = requests.get('https://api.openweathermap.org/data/2.5/weather?q=Oslo&appid=d2a2b4ae87b93c165c5421cee9970939&units=metric')
+        temp_owm = response_owm.json()['main']['temp']
+
+        # Compare temperatures
+        self.assertAlmostEqual(temp_custom, temp_owm, places=1, msg="Temperatures do not match")
